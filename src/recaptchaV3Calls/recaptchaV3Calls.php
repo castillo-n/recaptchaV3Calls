8696e255207b5f5793e22c7e6edd925ed0c524e2
Nelson <castillo-n@users.noreply.github.com>
2023-10-13 13:14:01 -0500

    Update recaptchaV3Calls.php
    return strings to echo directly into the code

--- src/recaptchaV3Calls/recaptchaV3Calls.php
+++ src/recaptchaV3Calls/recaptchaV3Calls.php
@@ -47,7 +47,7 @@ class recaptchaV3
         if ($key === null) {
             $key = $secret_key;
         }
-        echo "<script src='https://www.google.com/recaptcha/api.js?render=".$key."'></script>".
+        return "<script src='https://www.google.com/recaptcha/api.js?render=".$key."'></script>".
             "<script>".
             "    grecaptcha.ready(function() {".
             "        grecaptcha.execute('".$key."', {action:'validate_captcha'})".
@@ -59,7 +59,7 @@ class recaptchaV3
     }
     public static function htmlInsideTheFormCallInsert($key=null){
         //you don't really need to add this code to your page, you can just copy and paste the code and be good. 
-        echo "<input type='hidden' id='g-recaptcha-response' name='g-recaptcha-response'>".
+        return "<input type='hidden' id='g-recaptcha-response' name='g-recaptcha-response'>".
         "<input type='hidden' name='action' value='validate_captcha'>";
     }
 }
