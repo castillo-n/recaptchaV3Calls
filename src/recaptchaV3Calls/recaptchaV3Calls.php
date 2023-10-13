df6db4502748f4c934b026e8a22c585df6b48262
Nelson <castillo-n@users.noreply.github.com>
2023-10-13 12:29:42 -0500

    Update recaptchaV3Calls.php
    last section, html insert

--- src/recaptchaV3Calls/recaptchaV3Calls.php
+++ src/recaptchaV3Calls/recaptchaV3Calls.php
@@ -58,5 +58,8 @@ class recaptchaV3
             "</script>";
     }
     public static function htmlInsideTheFormCallInsert($key=null){
+        //you don't really need to add this code to your page, you can just copy and paste the code and be good. 
+        echo "<input type='hidden' id='g-recaptcha-response' name='g-recaptcha-response'>".
+        '<input type='hidden' name='action' value='validate_captcha'>";
     }
 }
